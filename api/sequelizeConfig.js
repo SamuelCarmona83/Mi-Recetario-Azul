8261a0a6b517f0ef5b78e491f8004f736024cac0
Samuel Carmona <samuel.carmona.rodrigz@gmail.com>
2024-12-23 02:32:45 +0000

    :bug: Fixing a bug in the sequelizeConfig.js file

--- api/sequelizeConfig.js
+++ api/sequelizeConfig.js
@@ -3,6 +3,14 @@
 import { Sequelize } from 'sequelize';
 const databaseUrl = process.env.DATABASE_URL || 'postgres://postgres:postgres@localhost:5432/example'
 
-const sequelize = new Sequelize(databaseUrl);
+const sequelize = new Sequelize(databaseUrl, {
+    dialect: "postgres",
+    dialectOptions: {
+      ssl: {
+        require: true,
+        rejectUnauthorized: false,
+      },
+    }
+});
 
 export default sequelize;
